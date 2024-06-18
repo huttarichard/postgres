4acb4cb35e7537efe72ea0bc4581482014b32984
Richard Hutta <huttarichard@gmail.com>
2024-06-18 12:28:41 +0200

    Error is not reported when .origin property does not exists.
    error: TypeError: Cannot read properties of undefined (reading 'replace')
        at queryError (file:///home/runner/.cache/deno/npm/registry.npmjs.org/postgres/3.4.4/src/connection.js:389:48)
        at errored (file:///home/runner/.cache/deno/npm/registry.npmjs.org/postgres/3.4.4/src/connection.js:384:17)
        at Socket.error (file:///home/runner/.cache/deno/npm/registry.npmjs.org/postgres/3.4.4/src/connection.js:376:5)

--- src/connection.js
+++ src/connection.js
@@ -385,8 +385,9 @@ function Connection(options, queues = {}, { onopen = noop, onend = noop, onclose
   }
 
   function queryError(query, err) {
+    const origin = query.origin || ''
     Object.defineProperties(err, {
-      stack: { value: err.stack + query.origin.replace(/.*\n/, '\n'), enumerable: options.debug },
+      stack: { value: err.stack + origin.replace(/.*\n/, '\n'), enumerable: options.debug },
       query: { value: query.string, enumerable: options.debug },
       parameters: { value: query.parameters, enumerable: options.debug },
       args: { value: query.args, enumerable: options.debug },
